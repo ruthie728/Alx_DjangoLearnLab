642678f6bdf53756ae80b323d4a88bf4da751e33
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-11-09 11:14:08 +0300

    Completed role-based access and custom permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Book, Library, UserProfile, Author
 
 # ==============================
