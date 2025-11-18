2217c3d354d40ddaeafc124a75a6c4af6c11b4ad
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-11-18 22:59:09 +0300

    Add ExampleForm required by tests

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,4 +5,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
-        
+
+
+# Add this to fix the test
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
