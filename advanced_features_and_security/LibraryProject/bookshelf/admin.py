f18a97b0e036d657b51fdfb4b72f68772928bfae
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-11-18 22:31:05 +0300

    Added CustomUser to bookshelf/models and updated settings

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
+# ---------------------------
+# Book Admin
+# ---------------------------
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # show columns in list view
-    search_fields = ('title', 'author')                     # allow search
-    list_filter = ('publication_year',)                     # add filter sidebar
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
 
 admin.site.register(Book, BookAdmin)
+
+# ---------------------------
+# Custom User Admin
+# ---------------------------
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': (
+            'is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions'
+        )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
