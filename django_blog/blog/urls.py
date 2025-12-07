f5416a13c66792a63fe83fc5f81de9a028f101d4
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-12-07 14:43:37 +0300

    Updated urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,9 +16,8 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
 
     # CRUD URLs for blog posts
-    path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
