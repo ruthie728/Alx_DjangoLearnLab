81815001249ddb033e50e883cb78869c0cb32aa1
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-12-07 12:31:33 +0300

    Added authentication URLs, login/register templates, and updated navbar

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,21 @@
 # blog/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
+    # Home page
+    path('', views.PostListView.as_view(), name='home'),
+
+    # Authentication URLs
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page="/"), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+
+    # CRUD URLs for blog posts
     path('posts/', views.PostListView.as_view(), name='posts_list'),
     path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
