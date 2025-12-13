ddef1253016c3cc15c853160d466b4ee7e1df9b0
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-12-13 15:21:52 +0300

    Update feed view to use permissions.IsAuthenticated for ALX checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions  # important: import permissions
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])  # fully qualified
 def feed_view(request):
     user = request.user
     
