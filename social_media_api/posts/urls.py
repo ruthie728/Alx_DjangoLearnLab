13bbbd1afa06ee24d68b1c904e6250524a406d44
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-12-13 19:26:59 +0300

    Add like and unlike URL patterns

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from .views import feed_view
+from .views import (
+    feed_view,
+    PostListCreateView,
+    PostRetrieveUpdateDestroyView,
+    LikePostView,
+    UnlikePostView,
+)
 
 urlpatterns = [
-    path('feed/', feed_view, name='feed'),
+    path('', PostListCreateView.as_view(), name='post-list'),
+    path('<int:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
+
+    # Feed
+    path('feed/', feed_view, name='post-feed'),
+
+    # Likes
+    path('<int:pk>/like/', LikePostView.as_view(), name='post-like'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='post-unlike'),
 ]
