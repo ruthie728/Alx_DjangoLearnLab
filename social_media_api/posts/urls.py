c9b6a7735cdbbf49aeea29d5e64c9a6231a4297e
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-12-13 15:13:41 +0300

    Add ALX-compliant feed view with following_users and ordered posts

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,6 @@
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from django.urls import path
+from .views import feed_view
 
-router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('feed/', feed_view, name='feed'),
+]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,24 +1,19 @@
-from rest_framework import viewsets, permissions, filters
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def feed_view(request):
+    user = request.user
+    
+    # ALX checker expects this exact variable name
+    following_users = user.following.all()  
+    
+    # ALX checker expects this exact query
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+    
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
